cdf0b5d0d23a06b6a9cbcec52ed171811bd5848d
Ashishkun110398 <ashishtripathi110398@gmail.com>
2026-09-13 00:20:16 +0530

    fundamentals

---
+++ variable.js
@@ -0,0 +1,56 @@
+let firstName = "Ashish"; // first name of a person
+let lastName = "Tripathi"; // last name of a person
+let country = "Bharat"; // country
+let city = "Delhi"; // capital city
+let age = 20; // age in years
+let isMarried = false;
+
+console.log(firstName, lastName, country, city, age, isMarried)
+
+
+// Declaring variables with number values
+
+const gravity = 9.81; // earth gravity  in m/s2
+const boilingPoint = 100; // water boiling point, temperature in oC
+const PI = 3.14; // geometrical constant
+
+// Variables can also be declaring in one line separated by comma
+
+let name = "Ashish", //name of a person
+  job = "student",
+  live = "Delhi";
+
+// To check the data type of a certain variable, we use the typeof operator. See the following example.
+
+console.log(typeof "Asabeneh"); // string
+
+console.log(typeof 5); // number
+console.log(typeof true); // boolean
+console.log(typeof null); // object type
+console.log(typeof undefined); // undefined
+
+
+
+/*
+
+Declaring different variables of different data types
+
+
+
+let firstName = 'Asabeneh' // first name of a person
+let lastName = 'Yetayeh' // last name of a person
+let country = 'Finland' // country
+let city = 'Helsinki' // capital city
+let age = 100 // age in years
+let isMarried = true 
+
+
+*/
+
+// Variables can also be declaring in one line separated by comma, however I recommend to use a seperate line to make code more readble
+ example 
+ 
+ 
+ /*let name = 'Asabeneh', job = 'teacher', live = 'Finland'
+console.log(name, job, live)  */
+
